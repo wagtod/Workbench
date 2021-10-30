565c9e8b938f5c504962db42cd0ff4341e4d51ca
wagtod <github.wagtod@neverbox.com>
2021-10-30 17:44:54 -0500

    Initial load

---
+++ Assets/Scripts/RotateBarrel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBarrel : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = Input.mousePosition - pos;
+            float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) -90;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+    }
+}
